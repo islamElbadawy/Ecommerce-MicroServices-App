3e88199e57700b2000b0739175d28d7016afaf20
islamElbadawy <islam.201211@gmail.com>
2025-12-08 11:19:49 +0300

    Create shared class lib with mediatr and CQRS patterns and interfaces

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,40 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+namespace CatalogAPI.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>
+{
+
+};
+
+public record CreateProductResult(Guid Id)
+{
+};
+
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Create Product Entity from Command object 
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+
+        // Todo
+        //Save to Database
+
+
+        // Return CreateProductResult with new Product Id
+        return new CreateProductResult(Guid.NewGuid()); 
+        throw new NotImplementedException();
+    }
+}
+
